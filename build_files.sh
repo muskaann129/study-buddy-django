3e9ab217620c5c6d65e5ce1f4cab3b9feeb85c55
Mohit Khatri <mohit4bug@gmail.com>
2024-01-27 01:00:29 +0530

    Hoja bhai

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,12 @@
 echo "BUILD START"
 echo "`python3 --version`"
+sudo apt-get install build-essential libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev tk-dev libffi-dev
+wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz
+cd Python-3.10.4
+./configure --enable-optimizations --with-ensurepip=install
+make -j $(nproc)
+sudo make altinstall
+tar -xvf Python-3.10.4.tgz
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -r requirements.txt

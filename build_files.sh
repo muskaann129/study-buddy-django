99ad6f1597d5841447a9faa916dc9d4540f12d9c
Mohit Khatri <mohit4bug@gmail.com>
2024-01-26 23:48:34 +0530

    Just checking vercel hahahahaha

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 echo "BUILD START"
+echo "`python3 --version`"
 python3.10 -m pip install -r requirements.txt
 python3.10 manage.py collectstatic --noinput --clear
 echo "BUILD END, I LOVE YOU MOHIT BHAIYA/COPILOT"

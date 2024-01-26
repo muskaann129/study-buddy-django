9bcd65d8ef796783d4b7a322bc0544066dffe5eb
Mohit Khatri <mohit4bug@gmail.com>
2024-01-26 23:53:51 +0530

    Fixed everything (hope so)

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 echo "BUILD START"
 echo "`python3 --version`"
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
 echo "BUILD END, I LOVE YOU MOHIT BHAIYA/COPILOT"
